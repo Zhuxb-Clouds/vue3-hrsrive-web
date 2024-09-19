0ab82cdd78f60fc2fa0d49c707b13025f4029d11
Zhuxb <ZhuxbClouds@gmail.com>
2024-09-19 22:53:18 +0800

    Refactor index.html: Update favicon link and add performance measurement script
    Refactor vite.config.ts: Update vite compression plugin configuration

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,11 @@ import viteCompression from 'vite-plugin-compression'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), svgLoader(), viteCompression({
-    threshold: 1024000 // 对大于 1mb 的文件进行压缩
+    threshold: 1025, // 对大于 1mb 的文件进行压缩
+    algorithm: 'gzip', // 使用 gzip 压缩
+    ext: '.gz', // 压缩后的文件后缀名
+    deleteOriginFile: false, // 是否删除原文件
+
   })],
   resolve: {
     alias: {
@@ -18,6 +22,15 @@ export default defineConfig({
     manifest: true,
     sourcemap: false,
     reportCompressedSize: true,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
   },
   esbuild: {
     drop: process.env.NODE_ENV === 'production' ? ['console', 'debugger'] : [],
